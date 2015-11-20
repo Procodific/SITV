fe95e86b257d0c7768b5bfae73c4a34eb1549c93
Eric <suericgp@gmail.com>
2015-11-20 03:10:18 -0600

    Ya hace prediccion y regresa latitud y longitud

--- SITV/AGSMapViewController.swift
+++ SITV/AGSMapViewController.swift
@@ -23,6 +23,9 @@ class AGSMapViewController: AGSMapView, AGSMapViewLayerDelegate, AGSLocatorDeleg
     var queue:NSOperationQueue! // Ejecuta el servicio web en background
     var access_token: NSString! // Access_token a obtener de la aplicación ArcGIS
     
+    var showAllResults : Bool = true
+    var firstGraphic : Bool = true;
+    
     // OUTLETS DEL UIVIEW
     @IBOutlet weak var directionsLabel: UILabel!
     @IBOutlet weak var prevBtn: UIBarButtonItem!
@@ -31,12 +34,16 @@ class AGSMapViewController: AGSMapView, AGSMapViewLayerDelegate, AGSLocatorDeleg
     @IBOutlet weak var originSearchBar: UISearchBar!
     @IBOutlet weak var destinationSearchBar: UISearchBar!
     
+    // GOOGLE MAPS
     var autocompleteTableView : UITableView!
     var lugares : Array<String> = []
+    let baseURLGeocode = "https://maps.googleapis.com/maps/api/geocode/json?"
+    var lookupAddressResults: Dictionary<NSObject, AnyObject>!
+    var fetchedFormattedAddress: String!
+    var fetchedAddressLongitude: Double!
+    var fetchedAddressLatitude: Double!
 
-    var showAllResults : Bool = true
-    var firstGraphic : Bool = true;
-    
+    // ARCGIS
     var graphicLayer: AGSGraphicsLayer! // Layer de todos los gráficos
     var locator: AGSLocator! // Locator de ArcGIS
     var calloutTemplate: AGSCalloutTemplate!
@@ -364,7 +371,49 @@ class AGSMapViewController: AGSMapView, AGSMapViewLayerDelegate, AGSLocatorDeleg
     }
     
     func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        let text = lugares[indexPath.row];
         
+        self.geocodeAddress(text, withCompletionHandler: { (status, success) -> Void in
+            
+        })
+    }
+    
+    func geocodeAddress(address: String!, withCompletionHandler completionHandler: ((status: String, success: Bool) -> Void)) {
+        if let lookupAddress = address {
+            var geocodeURLString = baseURLGeocode + "address=" + lookupAddress
+            geocodeURLString = geocodeURLString.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
+            
+            let geocodeURL = NSURL(string: geocodeURLString)
+            
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                let geocodingResultsData = NSData(contentsOfURL: geocodeURL!)
+                
+                do {
+                    let dictionary: Dictionary<NSObject, AnyObject> = try NSJSONSerialization.JSONObjectWithData(geocodingResultsData!, options: NSJSONReadingOptions.MutableContainers) as! Dictionary<NSObject, AnyObject>
+                    
+                    // Get the response status.
+                    let status = dictionary["status"] as! String
+                    
+                    if status == "OK" {
+                        let allResults = dictionary["results"] as! Array<Dictionary<NSObject, AnyObject>>
+                        self.lookupAddressResults = allResults[0]
+                        
+                        // Keep the most important values.
+                        self.fetchedFormattedAddress = self.lookupAddressResults["formatted_address"] as! String
+                        let geometry = self.lookupAddressResults["geometry"] as! Dictionary<NSObject, AnyObject>
+                        self.fetchedAddressLongitude = ((geometry["location"] as! Dictionary<NSObject, AnyObject>)["lng"] as! NSNumber).doubleValue
+                        self.fetchedAddressLatitude = ((geometry["location"] as! Dictionary<NSObject, AnyObject>)["lat"] as! NSNumber).doubleValue
+                        
+                        completionHandler(status: status, success: true)
+                    }
+                    else {
+                        completionHandler(status: status, success: false)
+                    }
+                } catch {
+                    completionHandler(status: "", success: false)
+                }
+            })
+        }
     }
     
     // Botón prev
